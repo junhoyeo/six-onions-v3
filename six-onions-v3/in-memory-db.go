eb5a4fd9a8005365a1646a26f917664407efe5b6
Junho Yeo <i@junho.io>
2023-10-03 00:38:01 +0900

    Fix naming inside `InMemoryDB`

--- six-onions-v3/in-memory-db.go
+++ six-onions-v3/in-memory-db.go
@@ -6,52 +6,57 @@ import (
 	"sync"
 )
 
+// InMemoryDB holds the mappings between OnionV3 addresses and IPv6 addresses.
 type InMemoryDB struct {
-	data        map[string]string // OnionV3Address to IPv6 mapping
-	reverseData map[string]string // IPv6 to OnionV3Address mapping
+	data        map[string]string // OnionV3 to IPv6 mapping
+	reverseData map[string]string // IPv6 to OnionV3 mapping
 	mu          sync.RWMutex
-	lastAddress [16]byte
+	lastIPv6    [16]byte
 }
 
+// NewInMemoryDB initializes a new InMemoryDB instance.
 func NewInMemoryDB() *InMemoryDB {
 	// Set initial address
-	initialAddress := [16]byte{0x2a, 0x0c, 0x2f, 0x07, 0xFE, 0xD5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	initialIPv6 := [16]byte{0x2a, 0x0c, 0x2f, 0x07, 0xFE, 0xD5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 	return &InMemoryDB{
 		data:        make(map[string]string),
 		reverseData: make(map[string]string),
-		lastAddress: initialAddress,
+		lastIPv6:    initialIPv6,
 	}
 }
 
-func (db *InMemoryDB) Set(OnionV3Address, IPv6 string) {
+// Set adds a mapping between the provided OnionV3 and IPv6 addresses.
+func (db *InMemoryDB) Set(OnionV3, IPv6 string) {
 	db.mu.Lock()
 	defer db.mu.Unlock()
 
-	db.data[OnionV3Address] = IPv6
-	db.reverseData[IPv6] = OnionV3Address
+	db.data[OnionV3] = IPv6
+	db.reverseData[IPv6] = OnionV3
 }
 
+// NextIPv6 generates the next IPv6 address sequentially.
 func (db *InMemoryDB) NextIPv6() net.IP {
 	db.mu.Lock()
 	defer db.mu.Unlock()
 
-	// Increment the last 64 bits of the address to get the next address
+	// Increment the last 64 bits of the address to get the next address.
 	// (assuming you only want to increment the host portion of the address)
-	nextValue := binary.BigEndian.Uint64(db.lastAddress[8:]) + 1
-	binary.BigEndian.PutUint64(db.lastAddress[8:], nextValue)
+	nextValue := binary.BigEndian.Uint64(db.lastIPv6[8:]) + 1
+	binary.BigEndian.PutUint64(db.lastIPv6[8:], nextValue)
 
-	// Copy the last address to ensure the underlying array isn't modified outside this method
-	nextAddress := make(net.IP, net.IPv6len)
-	copy(nextAddress, db.lastAddress[:])
+	// Copy the last address to ensure the underlying array isn't modified outside this method.
+	nextIPv6 := make(net.IP, net.IPv6len)
+	copy(nextIPv6, db.lastIPv6[:])
 
-	return nextAddress
+	return nextIPv6
 }
 
-func (db *InMemoryDB) GetByIPv6(IPv6 string) (string, bool) {
+// GetByIPv6 retrieves the OnionV3 address mapped to the provided IPv6 address.
+func (db *InMemoryDB) GetByIPv6(IPv6 string) (OnionV3 string, ok bool) {
 	db.mu.RLock()
 	defer db.mu.RUnlock()
 
-	OnionV3Address, ok := db.reverseData[IPv6]
-	return OnionV3Address, ok
+	OnionV3, ok = db.reverseData[IPv6]
+	return
 }
