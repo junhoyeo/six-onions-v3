04ab0d541426e55cb9bc23e725e0703602446708
Junho Yeo <i@junho.io>
2023-10-03 00:17:02 +0900

    DNS/TCP handlers to run in parallel

---
+++ six-onions-v3/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/base32"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"golang.org/x/net/proxy"
+)
+
+func main() {
+	go dnsHandler()
+	go tcpRequestHandler()
+
+	select {} // Block forever to keep the program running
+}
+
+func handleConn(c *net.TCPConn) {
+	// first, let's recover the address
+	tc, fd, err := realServerAddress(c)
+	defer c.Close()
+	defer fd.Close()
+
+	if err != nil {
+		log.Printf("Unable to recover address %s", err.Error())
+		return
+	}
+
+	toraddr := tc.IP[6:]
+	toronionaddr :=
+		fmt.Sprintf("%s.onion", base32.StdEncoding.EncodeToString(toraddr))
+
+	if !isAllowedPort(tc.Port) {
+		log.Printf("Disallowed connection from %s to %s:%d due to port block",
+			c.RemoteAddr().String(), toronionaddr, tc.Port)
+		return
+	}
+
+	log.Printf("Connection from %s to %s:%d",
+		c.RemoteAddr().String(), toronionaddr, tc.Port)
+
+	d, err := proxy.SOCKS5("tcp", "localhost:9050", nil, proxy.Direct)
+	if err != nil {
+		log.Printf("Unable to recover address %s", err.Error())
+		return
+	}
+
+	torconn, err := d.Dial("tcp", fmt.Sprintf("%s:%d", toronionaddr, tc.Port))
+	if err != nil {
+		log.Printf("Tor conncetion error %s", err.Error())
+		return
+	}
+
+	go io.Copy(torconn, fd)
+	io.Copy(fd, torconn)
+}
---
+++ six-onions-v3/tcp-request-handler.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func tcpRequestHandler() {
+	tport := flag.Int("transport", 1337,
+		"the port that iptables will be redirecting connections to")
+	flag.Parse()
+
+	la, _ := net.ResolveTCPAddr("tcp6", fmt.Sprintf("[::]:%d", *tport))
+	l, err := net.ListenTCP("tcp6", la)
+	if err != nil {
+		log.Fatalf("Unable to listen on the transparent port %s",
+			err.Error())
+	}
+
+	failurecount := 0
+	for {
+		c, err := l.AcceptTCP()
+		if err != nil {
+			if failurecount != 50 {
+				failurecount++
+			} else {
+				log.Printf("Unable to accept connection! %s", err.Error())
+			}
+			time.Sleep(time.Millisecond * time.Duration(failurecount*10))
+			continue
+		}
+		failurecount = 0
+
+		go handleConn(c)
+	}
+}
